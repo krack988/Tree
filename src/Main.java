cedcc16a87114ccaa704f71935d2defb09c935aa
krack988 <krack988@gmail.com>
2015-08-27 21:42:54 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 
 public class Main {
-    public static void main(String args[]){
-        boolean sun=true;
-        Size s=new Size(12,1);
-        Noise n=new Noise(3,7,6);
-        Shadow sh=new Shadow(sun);
-        Dry d=new Dry(58);
+    public static void main(String args[]) {
+        boolean sun = true;
+        Size s = new Size(12, 1);
+        Noise n = new Noise(3, 7, 6);
+        Shadow sh = new Shadow(sun);
+        Dry d = new Dry(58);
+        Tree t = new Tree("Topol", n, s, sh, d);
 
+        System.out.println("It's tree is "+t.vid);
+        System.out.println("Razmery: hight is-"+s.height+" girth-"+s.girth);
 
+        t.getNoise();
+        t.getShadow();
+        t.getDry();
 
 
     }
--- src/Tree.java
+++ src/Tree.java
@@ -4,15 +4,52 @@ public class Tree {
     Noise treeNoise;
     Size treeSize;
     Shadow treeShadow;
+    Dry treeDry;
 
-    public void getNoise(){
-        if (treeNoise.wind>4){
-            if (treeNoise.crona>5){
-                if (treeNoise.poraGoda>3&treeNoise.poraGoda<10){
-                    System.out.println("Tree noise!!!");
-                }
-            }else{System.out.println("Tree is little!!!");}
-        }else {System.out.println("Wind is low");}
+    Tree(String vid, Noise treeNoise, Size treeSize, Shadow treeShadow, Dry treeDry) {
+        this.vid = vid;
+        this.treeNoise = treeNoise;
+        this.treeSize = treeSize;
+        this.treeShadow = treeShadow;
+        this.treeDry = treeDry;
+
     }
+
+    public void getNoise() {
+        if (treeNoise.wind > 4) {
+            if (treeNoise.crona > 5) {
+                if (treeNoise.poraGoda > 3 & treeNoise.poraGoda < 10) {
+                    System.out.println("Tree noise!!!");
+                } else {
+                    System.out.println("Holodno");
+                }
+            } else {
+                System.out.println("Tree is little. No noise.");
+            }
+        } else {
+            System.out.println("Wind is low. No noise");
+        }
+    }
+
+    public void getShadow() {
+        if (treeShadow.shine == true) {
+            if (treeSize.height > 5 && treeSize.girth > 1) {
+                System.out.println("We have shadow!!!");
+            } else {
+                System.out.println("Tree is small. No shadow.");
+            }
+        } else {
+            System.out.println("The sun is not shine(");
+        }
+    }
+
+    public void getDry() {
+        if (treeDry.age > 150) {
+            System.out.println("The tree is dry");
+        } else {
+            System.out.println("Tree is yang");
+        }
+    }
+
 
 }
